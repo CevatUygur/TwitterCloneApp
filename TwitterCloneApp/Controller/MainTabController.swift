881219c8a5a72137d6c5ce99f8888bb6db79f924
CevatUygur <cevatuygur@gmail.com>
2022-12-18 01:21:56 +0300

    first commit

---
+++ TwitterCloneApp/Controller/MainTabController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabController.swift
+//  TwitterCloneApp
+//
+//  Created by CEVAT UYGUR on 18.12.2022.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .systemPink
+
+    }
+    
+}
