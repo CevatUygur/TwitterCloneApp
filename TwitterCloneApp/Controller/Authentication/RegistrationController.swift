4446dc555afc7120a4798ca68a843a8df9c3fc6d
CevatUygur <cevatuygur@gmail.com>
2022-12-24 02:40:57 +0300

    Uploading user profile image added

--- TwitterCloneApp/Controller/Authentication/RegistrationController.swift
+++ TwitterCloneApp/Controller/Authentication/RegistrationController.swift
@@ -116,22 +116,32 @@ class RegistrationController: UIViewController {
         guard let fullname = fullnameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
         
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                print("DEBUG: Error is \(error.localizedDescription)")
-                return
-            }
-            
-            guard let uid = result?.user.uid else { return }
-            
-            let values = ["email": email, "username": username, "fullname": fullname]
-            let ref = Database.database().reference().child("users").child(uid)
-            
-            ref.updateChildValues(values) { (error, ref) in
-                print("DEBUG: Seuccessfully updated user information.")
+        storageRef.putData(imageData, metadata: nil) { (meta, error) in
+            storageRef.downloadURL { url, error in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let uid = result?.user.uid else { return }
+                    
+                    let values = ["email": email, "username": username, "fullname": fullname, "profileImageUrl": profileImageUrl]
+                    
+                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
+                        print("DEBUG: Successfully updated user information.")
+                    }
+                }
+                
             }
         }
+        
         
     }
     
---
+++ TwitterCloneApp/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TwitterCloneApp
+//
+//  Created by CEVAT UYGUR on 24.12.2022.
+//
+
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
