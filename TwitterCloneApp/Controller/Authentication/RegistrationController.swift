026eba9188660ac8764bce9799eabd4e3c22f066
CevatUygur <cevatuygur@gmail.com>
2022-12-24 15:01:38 +0300

    Refactored API Code Into Service Class

--- TwitterCloneApp/Controller/Authentication/RegistrationController.swift
+++ TwitterCloneApp/Controller/Authentication/RegistrationController.swift
@@ -116,32 +116,12 @@ class RegistrationController: UIViewController {
         guard let fullname = fullnameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
         
-        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
-        let filename = NSUUID().uuidString
-        let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
+        let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
         
-        storageRef.putData(imageData, metadata: nil) { (meta, error) in
-            storageRef.downloadURL { url, error in
-                guard let profileImageUrl = url?.absoluteString else { return }
-                
-                Auth.auth().createUser(withEmail: email, password: password) { result, error in
-                    if let error = error {
-                        print("DEBUG: Error is \(error.localizedDescription)")
-                        return
-                    }
-                    
-                    guard let uid = result?.user.uid else { return }
-                    
-                    let values = ["email": email, "username": username, "fullname": fullname, "profileImageUrl": profileImageUrl]
-                    
-                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
-                        print("DEBUG: Successfully updated user information.")
-                    }
-                }
-                
-            }
+        AuthService.shared.registerUSer(credentials: credentials) { (error, ref) in
+            print("DEBUG: Sign up successfull...")
+            print("DEBUG: Handle update user interface here...")
         }
-        
         
     }
     
