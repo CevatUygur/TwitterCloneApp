da4988ed4f187437bde174b9fbd1f02885f50f16
CevatUygur <cevatuygur@gmail.com>
2022-12-24 17:27:25 +0300

    Custom User Object Model created

--- TwitterCloneApp/API/UserService.swift
+++ TwitterCloneApp/API/UserService.swift
@@ -14,10 +14,12 @@ struct UserService {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
-            print("DEBUG: Snapshot is \(snapshot)")
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             
-            guard let username = dictionary["username"] as? String else { return }
+            let user = User(uid: uid, dictionary: dictionary)
+            print("DEBUG: Username is \(user.username)")
+            print("DEBUG: Fullname is \(user.fullname)")
         }
+        
     }
 }
