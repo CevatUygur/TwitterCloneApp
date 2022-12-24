7a6cec50f14be75565d025b76ac743bbd29eb4f0
CevatUygur <cevatuygur@gmail.com>
2022-12-25 01:50:54 +0300

    UI Setup and Custom Init added

--- TwitterCloneApp/Controller/UploadTweetController.swift
+++ TwitterCloneApp/Controller/UploadTweetController.swift
@@ -11,6 +11,8 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Properties
     
+    private let user: User
+    
     private lazy var actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .twitterBlue
@@ -26,15 +28,33 @@ class UploadTweetController: UIViewController {
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        return iv
+    }()
+    
     
     // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
     
-    
+   
     // MARK: - Selectors
     
     @objc func handleCancel() {
@@ -53,9 +73,15 @@ class UploadTweetController: UIViewController {
     // MARK: - Helpers
     
     func configureUI(){
-        
         view.backgroundColor = .white
+        configureNavigationBar()
         
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+    }
+    
+    func configureNavigationBar() {
         let appearanceNavBar = UINavigationBarAppearance()
         appearanceNavBar.configureWithOpaqueBackground()
         appearanceNavBar.backgroundColor = .white
