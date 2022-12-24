0554415c76e54e911dc6398fb326791b786fecc8
CevatUygur <cevatuygur@gmail.com>
2022-12-24 17:59:34 +0300

    SDWebImage installed and Passing user to Feed Controller added

--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'TwitterCloneApp' do
 	pod 'Firebase/Database'
 	pod 'Firebase/Storage'
 	pod 'Firebase/Auth'
+	pod 'SDWebImage', '~> 5.0'
 
 end
--- TwitterCloneApp/Controller/FeedController.swift
+++ TwitterCloneApp/Controller/FeedController.swift
@@ -11,6 +11,10 @@ class FeedController: UIViewController {
     
     // MARK: - Properties
     
+    var user: User? {
+        didSet { print("DEBUG: Did set user in feed controller...") }
+    }
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -28,6 +32,13 @@ class FeedController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+        
+        let profileImageView = UIImageView()
+        profileImageView.backgroundColor = .twitterBlue
+        profileImageView.setDimensions(width: 32, height: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }
     
 }
