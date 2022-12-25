5ad05ec7bcaaed42e85384a01c33fa97ee846edf
CevatUygur <cevatuygur@gmail.com>
2022-12-25 16:24:52 +0300

    Tweet Timestamp added

--- TwitterCloneApp/ViewModel/TweetViewModel.swift
+++ TwitterCloneApp/ViewModel/TweetViewModel.swift
@@ -16,6 +16,15 @@ struct TweetViewModel {
         return tweet.user.profileImageUrl
     }
     
+    var timestamp: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let now = Date()
+        return formatter.string(from: tweet.timestamp, to: now) ?? "2m"
+    }
+    
     var userInfoText: NSAttributedString {
         let title = NSMutableAttributedString(string: user.fullname,
                                               attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
@@ -23,6 +32,11 @@ struct TweetViewModel {
         title.append(NSAttributedString(string: " @\(user.username)",
                                         attributes: [.font: UIFont.systemFont(ofSize: 14),
                                                      .foregroundColor: UIColor.lightGray]))
+        
+        title.append(NSAttributedString(string: " ・ \(timestamp)",
+                                        attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                     .foregroundColor: UIColor.lightGray]))
+        
         return title
     }
     
