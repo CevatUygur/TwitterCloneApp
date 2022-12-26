627e3f80b52e07a936544ce50c727d5c7461ba0f
CevatUygur <cevatuygur@gmail.com>
2022-12-26 11:26:04 +0300

    Profile Header Filter Bar & Selection Animation implemented

--- TwitterCloneApp/View/ProfileHeader.swift
+++ TwitterCloneApp/View/ProfileHeader.swift
@@ -11,6 +11,8 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private let filterBar = ProfileFilterView()
+    
     private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .twitterBlue
@@ -73,10 +75,18 @@ class ProfileHeader: UICollectionReusableView {
         return label
     }()
     
+    private let underlineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        filterBar.delegate = self
         
         addSubview(containerView)
         containerView.anchor(top: topAnchor,
@@ -114,6 +124,18 @@ class ProfileHeader: UICollectionReusableView {
                                 paddingTop: 8,
                                 paddingLeft: 12,
                                 paddingRight: 12)
+        
+        addSubview(filterBar)
+        filterBar.anchor(left: leftAnchor,
+                         bottom: bottomAnchor,
+                         right: rightAnchor,
+                         height: 50)
+        
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor,
+                             bottom: bottomAnchor,
+                             width: frame.width / 3,
+                             height: 2)
     }
     
     required init?(coder: NSCoder) {
@@ -128,5 +150,20 @@ class ProfileHeader: UICollectionReusableView {
     
     @objc func handleEditProfileFollow() {
         
+    }
+}
+
+// MARK: - ProfileFilterViewDelegate
+
+extension ProfileHeader: ProfileFilterViewDelegate {
+    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
+        guard let cell =  view.collectionview.cellForItem(at: indexPath) as? ProfileFilterCell else {
+            return
+        }
+        
+        let xPosition = cell.frame.origin.x
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.frame.origin.x = xPosition
+        }
     }
 }
