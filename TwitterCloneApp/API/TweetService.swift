1ae54a0fbd051c9ff47e9fdbb95187f560cb29d4
CevatUygur <cevatuygur@gmail.com>
2022-12-31 18:26:19 +0300

    Fetching & Displaying replies added

--- TwitterCloneApp/API/TweetService.swift
+++ TwitterCloneApp/API/TweetService.swift
@@ -10,7 +10,7 @@ import Firebase
 struct TweetService {
     static let shared = TweetService()
     
-    func uploadTweet(caption: String, type: UploadTweetConfiguration, completion: @escaping(Error?, DatabaseReference) -> Void) {
+    func uploadTweet(caption: String, type: UploadTweetConfiguration, completion: @escaping(DatabaseCompletion)) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
     
         let values = ["uid": uid,
@@ -57,11 +57,28 @@ struct TweetService {
             REF_TWEETS.child(tweetID).observeSingleEvent(of: .value) { snapshot in
                 guard let dictionary = snapshot.value as? [String: Any] else { return }
                 guard let uid = dictionary["uid"] as? String else { return }
+                
                 UserService.shared.fetchUser(uid: uid) { user in
                     let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
                     tweets.append(tweet)
                     completion(tweets)
                 }
+            }
+        }
+    }
+    
+    func fetcReplies(forTweet tweet: Tweet, completion: @escaping([Tweet]) -> Void) {
+        var tweets = [Tweet]()
+        
+        REF_TWEET_REPLIES.child(tweet.tweetID).observe(.childAdded) { snapshot in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            guard let uid = dictionary["uid"] as? String else { return }
+            let tweetID = snapshot.key
+            
+            UserService.shared.fetchUser(uid: uid) { user in
+                let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
+                tweets.append(tweet)
+                completion(tweets)
             }
         }
     }
--- TwitterCloneApp/Controller/TweetController.swift
+++ TwitterCloneApp/Controller/TweetController.swift
@@ -15,6 +15,9 @@ class TweetController: UICollectionViewController {
     // MARK: - Properties
     
     private let tweet: Tweet
+    private var replies = [Tweet]() {
+        didSet { collectionView.reloadData() }
+    }
     
     
     // MARK: - Lifecycle
@@ -31,9 +34,18 @@ class TweetController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        
-        print("DEBUG: Tweet caption is \(tweet.caption)")
+        fetchReplies()
     }
+    
+    // MARK: - API
+    
+    func fetchReplies() {
+        TweetService.shared.fetcReplies(forTweet: tweet) { replies in
+            self.replies = replies
+        }
+    }
+    
+    // MARK: - Helpers
     
     func configureCollectionView() {
         collectionView.backgroundColor = .white
@@ -47,12 +59,13 @@ class TweetController: UICollectionViewController {
 
 extension TweetController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return replies.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
         
+        cell.tweet = replies[indexPath.row]
         return cell
     }
 }
