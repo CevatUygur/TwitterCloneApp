af4b2c2a0fc1f4a23a2b2493a6b11b4abbb5e8ee
CevatUygur <cevatuygur@gmail.com>
2023-01-13 23:50:48 +0300

    Show Tweets From Followed Users Only & Sort by Date Functions Added

--- TwitterCloneApp/Controller/FeedController.swift
+++ TwitterCloneApp/Controller/FeedController.swift
@@ -39,12 +39,24 @@ class FeedController: UICollectionViewController {
         navigationController?.navigationBar.isHidden = false
     }
     
+    // MARK: - Selectors
+    
+    @objc func handleRefresh() {
+        fetchTweets()
+    }
+    
     // MARK: - API
     
     func fetchTweets() {
+        collectionView.refreshControl?.beginRefreshing()
+        
         TweetService.shared.fetchTweets { tweets in
             self.tweets = tweets
             self.checkIfUserLikedTweets(tweets)
+            
+            self.tweets = tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            
+            self.collectionView.refreshControl?.endRefreshing()
         }
     }
     
@@ -69,6 +81,10 @@ class FeedController: UICollectionViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.setDimensions(width: 44, height: 44)
         navigationItem.titleView = imageView
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
 
     }
     
